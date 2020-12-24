7a3881323dcdaaf37d78af4629257c6cfd24fe66
vaibhavverma <vaibhav.verrma@gmail.com>
2020-12-24 09:52:07 +0530

    fixed operator error

--- Two Port Parameters/transferFunctionZ.m
+++ Two Port Parameters/transferFunctionZ.m
@@ -1,5 +1,5 @@
 % This function returns the value of I2/Vs, where I2 = current at port 2
 % and Vs = source voltage
 function x = transferFunctionZ(Z, Zs, ZL)
-x = Z(2,1)/(Z(1,2)*Z(2,1) - (Z(1,1)+Zs)*(Z(2,2)*ZL));
+x = Z(2,1)/(Z(1,2)*Z(2,1) - (Z(1,1)+Zs)*(Z(2,2)+ZL));
 end
