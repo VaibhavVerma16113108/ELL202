77e937dcd0a6d793724f5ff1fac1f3e5dd0ba631
VaibhavVerma16113108 <52669625+VaibhavVerma16113108@users.noreply.github.com>
2021-01-04 23:04:07 +0530

    Create checkpr.m

---
+++ CIrcuit Synthesis/checkpr.m
@@ -0,0 +1,24 @@
+function ok = checkpr(b, a)
+    ok = true;
+    [r,p,k] = residue(b,a);
+    for i = 1 : length(p)
+        if(real(p(i)) == 0)
+            if(~(r(i) >= 0))
+                ok = false;
+                break;
+            end
+        end
+    end
+    syms s
+    num = poly2sym(b, s);
+    den = poly2sym(a, s);
+    g = num / den;
+    assume(s, 'positive');
+    s = j*s;
+    g = subs(g, s);
+    g = simplify(g);
+    realpart = simplify(real(g));
+    ok = isAlways(realpart >= 0) & ok;
+end
+            
+        
